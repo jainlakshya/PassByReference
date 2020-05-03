febdab4373ffa64fbe4dcb721f39460ef150e179
jainlakshya <51348857+jainlakshya@users.noreply.github.com>
2020-05-03 04:59:00 -0400

    Update obj.java

--- obj.java
+++ obj.java
@@ -2,25 +2,12 @@ package learning;
 
 public class obj {
 	
-	private int one;
-	private int two;
+	public int one;
 	
-	public int getOne() {
-		return one;
-	}
-	public void setOne(int one) {
-		this.one = one;
-	}
-	public int getTwo() {
-		return two;
-	}
-	public void setTwo(int two) {
-		this.two = two;
-	}
 	
 	@Override
 	public String toString() {
-		return "obj [one=" + one + ", two=" + two + "]";
+		return "obj [one=" + one+ "]";
 	}
 
 }
